d995483ff0c39f0ee35ee9232947f50352548aab
root <root@warm-sweetness-1.localdomain>
2022-05-17 18:39:01 -0400

    Generate ChinaCIDR

--- gen/gencidr.sh
+++ gen/gencidr.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
-cd /Users/charlesgray/Desktop/服务器/Rules/gen
+cd /root/Rules/gen
 echo "payload:" > ChinaCIDR.txt
 curl -sSL http://ftp.apnic.net/stats/apnic/delegated-apnic-latest | awk -F '|' '/CN/&&/ipv4/ {print $4 "/" 32-log($5)/log(2)}' > cidr4.txt
 sed -i -e 's/^/  - '\''/' cidr4.txt
@@ -9,9 +9,8 @@ sed -i -e 's/^/  - '\''/' cidr6.txt
 sed -i -e 's/$/'\''/' cidr6.txt
 cat cidr4.txt >> ChinaCIDR.txt
 cat cidr6.txt >> ChinaCIDR.txt
-mv ChinaCIDR.txt /Users/charlesgray/Desktop/服务器/Rules/ChinaCIDR.txt
-cd /Users/charlesgray/Desktop/服务器/Rules
-git fetch
+mv ChinaCIDR.txt /root/Rules/ChinaCIDR.txt
+cd /root/Rules
 git add .
 git commit  -m "Generate ChinaCIDR"
 git push -u origin master
